9a332b706e372ca37ef7e78dc7c93dad3b245d72
ashutoshg14 <114325127+ashutoshg14@users.noreply.github.com>
2023-07-19 12:20:58 +0530

    Update assignment2_q2.py

--- assignment2_q2.py
+++ assignment2_q2.py
@@ -17,10 +17,10 @@ def main():
 
     # Check command-line arguments
     if len(sys.argv) not in [2, 3]:
-        sys.exit("Usage: python traffic.py C:\Ashutosh\SOC 2023\Assignment2 [Training.h5]")
+        sys.exit("Usage: python traffic.py data_directory [model.h5]")
 
     # Get image arrays and labels for all image files
-    images, labels = load_data(sys.argv[1])
+    images, labels = load_data(r"C:\Ashutosh\UG 22-26\SOC 2023\Assignment2\traffic")
 
     # Split data into training and testing sets
     labels = tf.keras.utils.to_categorical(labels)
@@ -45,6 +45,19 @@ def main():
 
 
 def load_data(data_dir):
+    """
+    Load image data from directory `data_dir`.
+
+    Assume `data_dir` has one directory named after each category, numbered
+    0 through NUM_CATEGORIES - 1. Inside each category directory will be some
+    number of image files.
+
+    Return tuple `(images, labels)`. `images` should be a list of all
+    of the images in the data directory, where each image is formatted as a
+    numpy ndarray with dimensions IMG_WIDTH x IMG_HEIGHT x 3. `labels` should
+    be a list of integer labels, representing the categories for each of the
+    corresponding `images`.
+    """
     images = []
     labels = []
 
@@ -62,6 +75,11 @@ def load_data(data_dir):
 
 
 def get_model():
+    """
+    Returns a compiled convolutional neural network model. Assume that the
+    `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
+    The output layer should have `NUM_CATEGORIES` units, one for each category.
+    """
     model = tf.keras.models.Sequential([
 
         tf.keras.layers.Conv2D(
@@ -78,7 +96,7 @@ def get_model():
 
 
         tf.keras.layers.Dense(128, activation="relu"),
-        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dropout(0.3),
 
     
         tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
